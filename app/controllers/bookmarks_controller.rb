1d206a113b62fce4db8fe971ec3f3aaa79b22b02
tsyan <anna.tsykalova@gmail.com>
2014-10-09 03:54:31 -0400

    refactored Bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,13 @@
 class BookmarksController < ApplicationController
+  before_action :find_bookmark, only: [:show, :edit, :update, :destroy]
 
   def index
     # first, find all the bookmarks from the database
     # Bookmark.all runs: SELECT * FROM bookmarks;
-    @bookmarks = Bookmark.all
+    @bookmarks = Bookmark.order(:title)
   end
 
   def show
-    @bookmark = Bookmark.find(params[:id])
   end
 
   def new
@@ -17,30 +17,38 @@ class BookmarksController < ApplicationController
   end
 
   def create
-    @bookmark = Bookmark.create(bookmark_params)
-    # now this object is saved in the database
-    redirect_to action: :index
+    @bookmark = Bookmark.new(bookmark_params)
+    # now this object exists but is not yet saved to the database
+    if @bookmark.save
+      redirect_to action: :index # redirect_to IS a new HTTP request
+    else
+      render :new # render is not a new HTTP request, otherwise we would create 2 new objects - render looks for the view template of the matching name
+    end
   end
 
   def edit
-    @bookmark = Bookmark.find(params[:id])
   end
 
   def update
-    @bookmark = Bookmark.find(params[:id])
-    @bookmark.update_attributes(bookmark_params)
-    redirect_to action: :index
+    if @bookmark.update_attributes(bookmark_params)
+      redirect_to action: :index
+    else
+      render :edit # render looks for the view template of the matching name
+    end
   end
 
   def destroy
     # first find the object we want to delete
-    @bookmark = Bookmark.find(params[:id])
     # destroy is better than delete because destroy will also remove any orphan rows from the database, as well as any child dependents
     @bookmark.destroy
     redirect_to action: :index
   end
 
   private
+
+  def find_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
 
   def bookmark_params
     params.require(:bookmark).permit(:url, :title, :comment, :favorite)
