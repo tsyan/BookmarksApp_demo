d201c5164ed4c1fa8ccb2672d023be0c64e4694c
tsyan <anna.tsykalova@gmail.com>
2014-10-09 00:55:04 -0400

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Bookmarks should have url, title, comment (optional), favorite (optional)
+
+Bookmark.create(
+  url: "http://generalassemb.ly",
+  title: "General Assembly",
+  comment: "Seems like this WDI thing might be interesting")
+
+Bookmark.create(
+  url: "http://facebook.com",
+  title: "Facebook",
+  comment: "Ah, the book of faces...")
+
+Bookmark.create(
+  url: "http://guides.rubyonrails.org",
+  title: "Rails Guides",
+  comment: "Probably our bible for the next few weeks",
+  favorite: true)
